1198d6d9d5fc96a03c8edbad634f43d94f9f0034
lclasser <lclasser@gmail.com>
2019-11-06 18:16:33 +0900

    about dialog link

--- Source/test_vs2017/Resource.h
+++ Source/test_vs2017/Resource.h
@@ -1,6 +1,6 @@
 ﻿//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by TestJson.rc
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// TestJson.rc에서 사용되고 있습니다.
 //
 #define IDM_ABOUTBOX                    0x0010
 #define IDD_ABOUTBOX                    100
@@ -12,15 +12,16 @@
 #define IDC_EDIT_OUTPUT                 1001
 #define IDC_EDIT_TEXT                   1002
 #define IDC_BUTTON_CONVERT              1003
+#define IDC_MFCLINK1                    1003
 #define IDC_BUTTON2                     1004
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_RESOURCE_VALUE        133
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_CONTROL_VALUE         1004
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
